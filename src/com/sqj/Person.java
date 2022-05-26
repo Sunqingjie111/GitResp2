9baaefd622d491cc8a410c0222b31542c5db01e5
sunqj <2724353506@qq.com>
2022-05-26 17:27:55 +0800

    在Person类中增加name,height属性---

---
+++ src/com/sqj/Person.java
@@ -0,0 +1,11 @@
+package com.sqj;
+
+/**
+ * @Auther: yichun
+ * @Date: 2022/5/26 - 05 - 26 - 17:26
+ * @Description: com.sqj
+ */
+public class Person {
+    private String name;
+    private double height;
+}
